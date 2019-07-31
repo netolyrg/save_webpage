8ae3833a590b51c42ee316ee2c4bc26486bd39f2
Александр Богданов <netolyrg@gmail.com>
2019-07-31 17:08:48 +0300

    now function makes one thing, any preconditions on user side

--- README.md
+++ README.md
@@ -1,83 +1,51 @@
 # Save web page
-Simple python function that let you save a whole web page using Selenium and Pillow libraries.
+Simple python function that provide you to take a whole web page screenshot from Selenium WebDriver using Pillow.
 
 ## Description
 
-This functions save pages to files (more than 30 formats available) and support cookies and browser options such as `--headless` and others.
+This functions save pages to files (more than 30 formats available).
 
 I need this function for testing purposes, but I think it can be useful in another tasks.
-
-It perfectly works in headless mode on macOS High Sierra 10.13 with Google Chrome 64.
-And works with headless mode on Windows 10 with Google Chrome 64.
-
+``
+It perfectly works in headless mode on macOS Mojave 10.14 with Google Chrome 75, 
+on macOS High Sierra 10.13 with Google Chrome 64 and on Windows 10 with Google Chrome 64.
 
 ## Usage
 #### Simple
 
 ```
-save_webpage(
-    "path/to/driver",
-    "https://google.com/",
-    "google.jpg"
-    )
+from save_webpage import save_webpage
+from selenium import webdriver
+
+
+driver = webdriver.Chrome('./chromedriver')
+driver.get('https://github.com/about')
+
+save_webpage(driver, 'page.jpg')
+
+driver.quit()
 ```
-
-it's simple usage of function. In your folder you found google.jpg image of whole google.com page.
-
-#### Additional browser options support
-
-```
-options = ["--headless", "--window-size=1280, 720"]
-
-save_webpage(
-    "path/to/driver",
-    "https://google.com/",
-    "google.jpg",
-    options=options
-    )
-```
-
-In this example chrome starts in headless mode with window size 1280x720.
 
 For **windows** in headless mode don't forget about `--disable-gpu` key.
-
-#### Cookies support
-
-```
-cookies = [
-    {
-        "name": "some_cookie_name",
-        "value": "some_cookie_value",
-        "domain": "some_domain.com"
-    }
-        ]
-
-save_webpage(
-    "path/to/driver",
-    "https://google.com/",
-    "google.jpg",
-    cookies=cookies
-    )
-```
-
-This example shows how to load page with cookies.
-
-Actually, it load page, add cookies and then reload page. This happens due to selenium specific behavior. For addition information about selenium and cookies see [docs](http://www.seleniumhq.org/docs/03_webdriver.jsp).
-
 #### PIL keywords
 
 ```
+
+
+from save_webpage import save_webpage
+from selenium import webdriver
+
 image_options = {
         "format": "JPEG",
         "quality": 80
     }
 
-save_webpage(
-    "path/to/driver",
-    "https://google.com/",
-    "google.jpg",
-    **image_options
-    )
+driver = webdriver.Chrome('./chromedriver')
+driver.get('https://github.com/about')
+
+save_webpage(driver, 'page.jpg', **image_options)
+
+driver.quit()
 ```
 
 In this example shows how to save image in JPEG format with 80 quality (default parameters).
@@ -87,7 +55,5 @@ This functionality provided by PIL library. For more information see [docs](http
 ## Other
 #### TODOs
 
-- [ ] Add other webdrivers support
-- [x] Fix image_options
-- [x] Make simple Retina (and other with big device pixel ratio) display support
+- [ ] Check other webdrivers support
 - [ ] Add inline use (?)
--- save_webpage.py
+++ save_webpage.py
@@ -1,98 +1,98 @@
 # -*- coding: utf-8 -*-
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from io import BytesIO
+
 from PIL import Image
 
-
-DEFAULT_IMAGE_FORMAT = "JPEG"
+DEFAULT_IMAGE_FORMAT = 'JPEG'
 DEFAULT_IMAGE_QUALITY = 80
-
 
 TIME_TO_WAIT = 6
 
 
-def save_webpage(driver_path, url, file_name, options=None, cookies=None, **image_options):
+def save_webpage(driver, file_name, **kwargs):
     """
 
-    :param driver_path: path to google chrome driver
-    :param url: valid url
+    :param driver: selenium driver object
     :param file_name:
-    :param options: browser options
-    :param cookies: list of additional cookies
-    :param image_options: keywords parameters to pillow save function
-    :type url: str
+    :param kwargs: keywords parameters to pillow save function
+
     :type file_name: str
-    :type cookies: list
-    :type image_options: dict
+
     :return: name of file
     """
-
-    # necessary javascript
-    device_pixel_ratio_js = "return window.devicePixelRatio"
-    scroll_height_js = "return document.body.scrollHeight"
-    inner_height_js = "return window.innerHeight"
-    scroll_to_js = "window.scrollTo(0, {})"
-
     # define necessary image properties
-    image_options["format"] = image_options.get("format") or DEFAULT_IMAGE_FORMAT
-    image_options["quality"] = image_options.get("quality") or DEFAULT_IMAGE_QUALITY
+    image_options = dict()
+    image_options['format'] = kwargs.get('format') or DEFAULT_IMAGE_FORMAT
+    image_options['quality'] = kwargs.get('quality') or DEFAULT_IMAGE_QUALITY
 
-    # if have browser options - just add it
-    chrome_options = Options()
-    if options:
-        for option in options:
-            chrome_options.add_argument(option)
-
-    driver = webdriver.Chrome(
-        executable_path=driver_path,
-        options=chrome_options
-    )
-
-    device_pixel_ratio = driver.execute_script(device_pixel_ratio_js)
+    device_pixel_ratio = get_device_pixel_ratio(driver)
 
     if device_pixel_ratio > 1:
-        old_width = driver.get_window_size()['width']
-        old_heidht = driver.get_window_size()['height']
-        driver.set_window_size(old_width//device_pixel_ratio, old_heidht//device_pixel_ratio)
+        resize_window(driver, device_pixel_ratio)
 
-    driver.get(url)
-
-    # set cookies
-    if cookies:
-        for cookie in cookies:
-            driver.add_cookie(cookie)
-
-        # we need to 'activate' cookies
-        driver.refresh()
-
-    driver.implicitly_wait(TIME_TO_WAIT)
-
-    inner_height = driver.execute_script(inner_height_js)
-    scroll_height = driver.execute_script(scroll_height_js)
+    inner_height = get_inner_height(driver)
+    scroll_height = get_scroll_height(driver)
 
     # hide scrollbar coz it's lame
-    driver.execute_script("document.documentElement.style.overflow = 'hidden'")
+    hide_scrollbar(driver)
 
-    actual_page_size = scroll_height*device_pixel_ratio
+    actual_page_size = scroll_height * device_pixel_ratio
 
-    slices = []
+    slices = make_screen_slices(driver, inner_height, scroll_height)
 
-    for offset in range(0, scroll_height+1, inner_height):
-        driver.execute_script(scroll_to_js.format(offset))
-        img = Image.open(BytesIO(driver.get_screenshot_as_png()))
-        slices.append(img)
-
-    # create image
-    screenshot = Image.new('RGB', (slices[0].size[0], actual_page_size))
-
-    for i, img in enumerate(slices[:-1]):
-        screenshot.paste(img, (0, i * inner_height * device_pixel_ratio))
-    else:
-        screenshot.paste(slices[-1], (0, (scroll_height - inner_height) * device_pixel_ratio))
-
-    screenshot.save(file_name, **image_options)
-    driver.quit()
+    glue_slices_into_image(slices, file_name, image_options, actual_page_size, device_pixel_ratio, inner_height,
+                           scroll_height)
 
     return file_name
+
+
+def glue_slices_into_image(slices, file_name, image_options, actual_page_size, device_pixel_ratio, inner_height,
+                           scroll_height):
+    image_file = Image.new('RGB', (slices[0].size[0], actual_page_size))
+
+    for i, img in enumerate(slices[:-1]):
+        image_file.paste(img, (0, i * inner_height * device_pixel_ratio))
+    else:
+        image_file.paste(slices[-1], (0, (scroll_height - inner_height) * device_pixel_ratio))
+
+    image_file.save(file_name, **image_options)
+
+
+def make_screen_slices(driver, inner_height, scroll_height):
+    slices = []
+    for offset in range(0, scroll_height + 1, inner_height):
+        scroll_to(driver, offset)
+        img = Image.open(BytesIO(driver.get_screenshot_as_png()))
+        slices.append(img)
+    return slices
+
+
+def get_scroll_height(driver):
+    scroll_height_js = 'return document.body.scrollHeight'
+    return driver.execute_script(scroll_height_js)
+
+
+def get_inner_height(driver):
+    inner_height_js = 'return window.innerHeight'
+    return driver.execute_script(inner_height_js)
+
+
+def get_device_pixel_ratio(driver):
+    device_pixel_ratio_js = 'return window.devicePixelRatio'
+    return driver.execute_script(device_pixel_ratio_js)
+
+
+def hide_scrollbar(driver):
+    hidden_scrollbar = 'document.documentElement.style.overflow = \"hidden\"'
+    driver.execute_script(hidden_scrollbar)
+
+
+def scroll_to(driver, offset):
+    scroll_to_js = 'window.scrollTo(0, {})'
+    driver.execute_script(scroll_to_js.format(offset))
+
+
+def resize_window(driver, device_pixel_ratio):
+    old_width = driver.get_window_size()['width']
+    old_height = driver.get_window_size()['height']
+    driver.set_window_size(old_width // device_pixel_ratio, old_height // device_pixel_ratio)
